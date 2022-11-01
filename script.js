c2ecfd8677609d3004771b19436cba19f5beb0fb
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-11-01 14:03:14 +0000

    Adjusted the height of section-2 mobile

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ $(document).ready(function(
     //event will occur once we click the moneyBox logo on mobile view
     //aim is to create a dropdown menu for mobile
     $('#moneybox_logo').click(function(){
-        
+
     })
 })
