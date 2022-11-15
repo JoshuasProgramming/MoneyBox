4c2daeef38da2b0e31cdec34853f1471b3c07cc5
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2022-11-15 13:34:14 +0000

    Started development of hamburger popup menu

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ $(document).ready(function(
     //event will occur once we click the moneyBox logo on mobile view
     //aim is to create a dropdown menu for mobile
     $('#moneybox_logo').click(function(){
-        
+        $('#popup').css("display", "inline");
     });
 })
